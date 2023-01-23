1dfe0495c0315567a9fb8a35beb55f2c23ae737a
Ikhtiar Adli Wicaksono <112399122+ikhtiar2217051060@users.noreply.github.com>
2023-01-23 14:07:15 +0700

    Update konversi biner.cpp

--- konversi biner.cpp
+++ konversi biner.cpp
@@ -2,18 +2,40 @@
 using namespace std;
 
 int main(){
-    int desimal; int batas=0; int sisa[100];
 
+    int desimal; int batas2=0; int batas8=0; int biner[100]; 
+    int oktal[100]; int perdua; int perlapan;
+    
     cin>>desimal;
 
+    perdua=desimal;
+    perlapan=desimal;
+
     do{
-        sisa[batas] = desimal % 2;
-        desimal = desimal / 2;
-        batas++;
-    }while(desimal>0);
+        biner[batas2] = perdua%2;
+        perdua = perdua/2;
+        batas2++;
+    }while(perdua>0);
+
+    do{
+        oktal[batas8]=perlapan%8;
+        perlapan=perlapan/8;
+        batas8++;
+    }while(perlapan>0);
+
+    cout<<endl;
+    cout<<"Biner\t: ";
     
-    for(int x=batas-1; x>=0; x--){
-        cout<<sisa[x];
+    for(int x=batas2-1; x>=0; x--){
+        cout<<biner[x];
     }
+    
+    cout<<endl;
+    cout<<"Oktal\t: ";
+    
+    for(int y=batas8-1; y>=0; y--){
+        cout<<oktal[y];
+    }
+
     return 0;
 }
