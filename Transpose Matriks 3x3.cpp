219778fe0d78d405b8bf06f8a7454ed7e7c3ccd4
Ikhtiar Adli Wicaksono <112399122+ikhtiar2217051060@users.noreply.github.com>
2023-02-08 01:03:40 +0700

    Update Transpose Matriks 3x3.cpp

--- Transpose Matriks 3x3.cpp
+++ Transpose Matriks 3x3.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main (){	
 	//deklarasi matriks sebagai array multi dimensi berukuran 3x3, 3 baris (ke samping) dan 3 kolom (ke bawah)
-	int matriks [3][3];
+	
+	int baris, kolom;
+	cin>>baris>>kolom;
+	int matriks [baris][kolom];
 	//proses looping dari indeks array ke-0 sampai indeks ke-2
-	for (int x=0; x<3; x++){
-		for (int y=0; y<3; y++){
+	for (int x=0; x<baris; x++){
+		for (int y=0; y<kolom; y++){
 			//output instruksi pengisian elemen matriks
 			cout<<"baris "<<x+1<<", kolom "<<y+1<<" = ";
 			//input elemen dari setiap baris dan kolom
@@ -17,8 +20,8 @@ int main (){
 	//output hasil matriks
 	cout<<"Hasil Matriks = "<<endl;
 	//inputan setiap elemen matriks yang tersimpan kemudian ditampilkan secara otomotatis dengan proses looping
-	for (int x=0; x<3; x++){
-		for (int y=0; y<3; y++){
+	for (int x=0; x<baris; x++){
+		for (int y=0; y<kolom; y++){
 			//output bentuk asli matriks
 			cout<<matriks[x][y]<<" ";
 		}
@@ -26,8 +29,8 @@ int main (){
 	}
 	cout<<"Hasil Transpose = "<<endl;
 	//pada case matriks 3x3 maka tidak perlu mengubah x < baris menjadi x < kolom, begitu juga dengan y < kolom menjadi y < baris
-	for (int x=0; x<3; x++){
-		for (int y=0; y<3; y++){
+	for (int x=0; x<kolom; x++){
+		for (int y=0; y<baris; y++){
 			//output matriks setelah ditranspose
 			cout<<matriks[y][x]<<" ";
 		}
