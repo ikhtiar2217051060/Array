2c7398d1926c10417015be8a64ddd99b5559dd9b
Ikhtiar Adli Wicaksono <112399122+ikhtiar2217051060@users.noreply.github.com>
2023-02-08 15:12:37 +0700

    Add files via upload

---
+++ hitungvokal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int vokal=0; string kata; 
+    cin>>kata;
+
+    for(int i=0; i<=kata.length(); i++){
+        if(kata[i]=='a'||kata[i]=='i'||kata[i]=='u'
+        ||kata[i]=='e'||kata[i]=='0'){
+            vokal++;
+        }
+    }
+    cout<<"Banyaknya huruf vokal dalam kata tsb: "<<vokal;
+    return 0;
+}
